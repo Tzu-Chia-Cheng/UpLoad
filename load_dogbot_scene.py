c013f75a8f3af530187ff95fe9baab4b9b470b5c
鄭子嘉 <tzuchia.cheng@syntecclub.com.tw>
2026-04-10 18:22:11 +0800

    稍微整理導入

--- load_dogbot_scene.py
+++ load_dogbot_scene.py
@@ -2,11 +2,9 @@ import omni
 import time
 import numpy as np
 from isaacsim import SimulationApp
-simulation_app = SimulationApp({
-    "headless": False,
-    "sync_loads": True,
-    "active_config": "default"
-})
+
+#It is mandated that it be written this way.
+simulation_app = SimulationApp( { "headless": False } )
 
 from isaacsim.core.utils.stage import add_reference_to_stage
 from isaacsim.core.api import World
@@ -15,45 +13,34 @@ from isaacsim.core.api.robots import Robot
 class Go2Sim():
     def __init__(self):   
         # initial settings
-        self.timeline = omni.timeline.get_timeline_interface()
-        self.timeline.set_auto_update(True)
+        self.Timeline = omni.timeline.get_timeline_interface()
         
-        self.world = World(physics_dt = 0.002, rendering_dt = 0.02, stage_units_in_meters=1)
-        self.world.add_physics_callback("callback_control", callback_fn=self.callback)
+        self.World = World( physics_dt = 0.002, rendering_dt = 0.02, stage_units_in_meters = 1 )
         
         # add surface and robot dog
-        self.world.scene.add_default_ground_plane()
-        self.robot_asset_path = "D:\\10102127\\go2_description\\urdf\\go2_description\\go2_description.usd"
-        self.prim_path = "/World/Go2"
-        add_reference_to_stage(usd_path=self.robot_asset_path, prim_path=self.prim_path)
+        self.World.scene.add_default_ground_plane()
+        self.RobotAssetPath = "D:\\10102127\\go2_description\\urdf\\go2_description\\go2_description.usd"
+        self.PrimPath = "/World/Go2"
+        add_reference_to_stage( usd_path = self.RobotAssetPath, prim_path = self.PrimPath )
 
-        self.Go2 = Robot(prim_path="/World/Go2", name="Go2", position=np.array([0.0, 0.0, 0.5]), orientation=np.array([0, 0, 0, 1]))
-        self.world.scene.add(self.Go2)
+        self.Go2 = Robot(prim_path = self.PrimPath, name = "Go2", position = np.array( [ 0.0, 0.0, 0.5 ] ) )
+        self.World.scene.add(self.Go2)
         
         #reset the world to default, ***SENSOR HAVE TO DECLARE BEFORE IT!!!***
-        self.world.reset()
-
-    def callback(self, step_size):
-        print(1)
+        self.World.reset()
 
     def run_simulation(self):
-        self.world.reset()
-        self.timeline.play()
-    
-        self.Go2.set_joint_positions(np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
-        start = 0
-        end = 0
+        self.World.reset()
+        self.Timeline.play()
+        InitPos = np.zeros( 12 )
+        self.Go2.set_joint_positions( InitPos )
+
         while simulation_app.is_running():
-            end = start
-            start = time.time()
-            print(start - end)
             #update simulation step
-            self.world.step(render=True)
-            
-            #check if delay is needed or not            
+            self.World.step(render=True)         
 
         # Cleanup
-        self.timeline.stop()
+        self.Timeline.stop()
         simulation_app.close()
 
 if __name__ == '__main__':
