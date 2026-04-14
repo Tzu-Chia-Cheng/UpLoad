6db04d8c6fc9f1e74a1fd0720c1eecdb1fce6e0c
鄭子嘉 <tzuchia.cheng@syntecclub.com.tw>
2026-04-14 16:43:07 +0800

    改動檔名

---
+++ isaacsim_config.py
@@ -0,0 +1,17 @@
+ROBOT_SCENE = "D:\\10102127\\go2_description\\urdf\\go2_description\\go2_description.usd"
+ROBOT_PRIM_PATH = "/World/Go2"
+
+DOMAIN_ID = 1 # domain id
+INTERFACE = "lo" # interface
+
+SIM_TIME_STEP = 0.02 # [sec]
+SIM_PHYSICS_DT = 0.001 # [sec]
+SIM_RENDER_DT = 0.02 # [sec]
+
+TOPIC_LOWCMD = "rt/lowcmd"
+TOPIC_LOWSTATE = "rt/lowstate"
+
+MOTOR_SENSOR_NUM = 3
+MOTOR_NUM_GO2 = 12
+IMU_SENSOR_DIM = 10
+MOTOR_SENSOR_DIM = MOTOR_SENSOR_NUM * MOTOR_NUM_GO2
